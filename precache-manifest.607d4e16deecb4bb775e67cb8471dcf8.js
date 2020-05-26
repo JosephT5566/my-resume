5b42a9ed74e83836487160287b2e65c62345c4ed
JosephTseng <zxp930110@hotmail.com.tw>
2020-05-26 16:17:57 +0800

    Updates

---
+++ precache-manifest.607d4e16deecb4bb775e67cb8471dcf8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0f4544f5798e2d28ca58fa11267ce25",
+    "url": "/my-resume.git/index.html"
+  },
+  {
+    "revision": "1db88d2ae2966faefb31",
+    "url": "/my-resume.git/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e3f0a82daac6e5415c08",
+    "url": "/my-resume.git/static/js/2.2141e1a8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-resume.git/static/js/2.2141e1a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1db88d2ae2966faefb31",
+    "url": "/my-resume.git/static/js/main.7932c8c7.chunk.js"
+  },
+  {
+    "revision": "760639c6b331b1644f37",
+    "url": "/my-resume.git/static/js/runtime-main.92f40fcb.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-resume.git/static/media/logo.5d5d9eef.svg"
+  }
+]);
