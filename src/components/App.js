145e3e34edfa313e1f3575112198856dce1d8321
JosephTseng <zxp930110@hotmail.com.tw>
2020-06-08 19:00:45 +0800

    Creating ResumeCard

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
+import ResumeCard from './ResumeCard';
+
 class App extends React.Component {
     render() {
         return (
             <div className="ui container">
-                Hello
+                <ResumeCard/>
             </div>
         );
     }
