87aa3397b16740ac8470b5731fc64a52122ec6e0
Joseph Tseng <zxp930110@hotmail.com.tw>
2020-06-09 13:52:12 +0800

    Adding personal info 1. Adding personal info into personalInfo 2. Styling personalInfo card 3. Styling App, adding margin-top

--- src/components/cards/personalInfo.js
+++ src/components/cards/personalInfo.js
@@ -1,3 +1,5 @@
+import './personalInfo.css'
+
 import React from 'react';
 
 import personalShot from '../../assets/images/personal_shot.jpg';
@@ -6,6 +8,14 @@ const personalInfo = (props) => {
     return (
         <div className="comment">
             <img src={personalShot} alt="personal photo" style={{width: "50%"}}/>
+            <div className="content">
+                <h1>曾俊霖</h1>
+                <hr/>
+                <p>1992/03/04</p>
+                <p>新竹縣竹北市</p>
+                <p>zxp930110@hotmail.com.tw</p>
+                <p>0928746335</p>
+            </div>
         </div>
     );
 };
