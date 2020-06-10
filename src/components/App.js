b50558d6dccb5c2ce65d6b956febc1d749ffc318
Joseph Tseng <zxp930110@hotmail.com.tw>
2020-06-10 17:35:46 +0800

    Adding ResumeCardList Class 1. Adding ResumeCardList Class in ResumeCard.js to handle all cards, instead in App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,26 +2,14 @@ import './App.css';
 import '../assets/fonts/jf-openhuninn-1.1/jf-openhuninn-1.1.ttf';
 
 import React from 'react';
-import ReactDom from 'react-dom';
 
-import ResumeCard from './ResumeCard';
-import PersonalInfo from './cards/personalInfo';
-import Expertise from './cards/expertise';
-import Works from './cards/works';
+import ResumeCardList from './ResumeCard';
 
 class App extends React.Component {
     render() {
         return (
             <div className="ui container">
-                <ResumeCard>
-                    <PersonalInfo />
-                </ResumeCard>
-                <ResumeCard>
-                    <Expertise />
-                </ResumeCard>
-                <ResumeCard>
-                    <Works />
-                </ResumeCard>
+                <ResumeCardList/>
             </div>
         );
     }
--- src/components/ResumeCard.js
+++ src/components/ResumeCard.js
@@ -1,9 +1,31 @@
 import React from 'react';
 
+import PersonalInfo from './cards/personalInfo';
+import Expertise from './cards/expertise';
+import Works from './cards/works';
+
 class ResumeCard extends React.Component {
     render() {
         return <div className="ui segment">{this.props.children}</div>;
     }
 }
 
-export default ResumeCard;
+class ResumeCardList extends React.Component {
+    render() {
+        return (
+            <div>
+                <ResumeCard>
+                    <PersonalInfo/>
+                </ResumeCard>
+                <ResumeCard>
+                    <Expertise/>
+                </ResumeCard>
+                <ResumeCard>
+                    <Works/>
+                </ResumeCard>
+            </div>
+        );
+    }
+}
+
+export default ResumeCardList;
