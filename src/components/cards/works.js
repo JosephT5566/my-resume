8ba81b5f7de1550229d89672c7cd5e66849b9df5
Joseph Tseng <zxp930110@hotmail.com.tw>
2020-06-10 21:13:10 +0800

    Showing different elements (work details) separately 1. creating new variables (visibleWork1~3) and functions (handleClickWork1~3) to handle different element behaviors 2. setting cursor to pointer

--- src/components/cards/works.js
+++ src/components/cards/works.js
@@ -6,12 +6,32 @@ import { CSSTransition } from 'react-transition-group';
 class Works extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { visible: false };
-        this.handleClick = this.handleClick.bind(this);
+        this.state = {
+            visibleWork1: false,
+            visibleWork2: false,
+            visibleWork3: false
+        };
+        this.handleClickWork1 = this.handleClickWork1.bind(this);
+        this.handleClickWork2 = this.handleClickWork2.bind(this);
+        this.handleClickWork3 = this.handleClickWork3.bind(this);
     }
 
-    handleClick() {
-        this.setState({ visible: !this.state.visible });
+    handleClickWork1() {
+        this.setState({ visibleWork1: !this.state.visibleWork1 });
+        this.setState({ visibleWork2: false });
+        this.setState({ visibleWork3: false });
+    }
+
+    handleClickWork2() {
+        this.setState({ visibleWork1: false });
+        this.setState({ visibleWork2: !this.state.visibleWork2 });
+        this.setState({ visibleWork3: false });
+    }
+
+    handleClickWork3() {
+        this.setState({ visibleWork1: false });
+        this.setState({ visibleWork2: false });
+        this.setState({ visibleWork3: !this.state.visibleWork3 });
     }
 
     render() {
@@ -22,13 +42,13 @@ class Works extends React.Component {
                     <p>
                         <i className="circle icon"></i>
                         <span
-                            onClick={this.handleClick} // or we can use arrow funcion directly
+                            onClick={this.handleClickWork1} // or we can use arrow funcion directly
                             style={{ fontSize: '22px' }}
                         >
                             憶正科技 (MemoRight), 應用工程師, Oct 2016 ~ Mar 2017
                             <CSSTransition
                                 classNames="memoRight cardTransition"
-                                in={this.state.visible}
+                                in={this.state.visibleWork1}
                                 timeout={300}
                                 unmountOnExit
                             >
@@ -41,19 +61,19 @@ class Works extends React.Component {
                     <p>
                         <i className="circle icon"></i>
                         <span
-                            onClick={this.handleClick}
+                            onClick={this.handleClickWork2}
                             style={{ fontSize: '22px' }}
                         >
                             晶兆科技 (Big-Inno), 應用工程師, May 2017 ~ Sep 2018
                             <CSSTransition
                                 classNames="biginno cardTransition"
-                                in={this.state.visible}
+                                in={this.state.visibleWork2}
                                 timeout={300}
                                 unmountOnExit
                             >
                                 <div className="transitionPanel">
-                                    Memory controller test with Magnum system 
-                                    <br/>
+                                    Memory controller test with Magnum system
+                                    <br />
                                     MP application development in C#
                                 </div>
                             </CSSTransition>
@@ -62,21 +82,21 @@ class Works extends React.Component {
                     <p>
                         <i className="circle icon"></i>
                         <span
-                            onClick={this.handleClick}
+                            onClick={this.handleClickWork3}
                             style={{ fontSize: '22px' }}
                         >
-                            富智銓科技, 韌體工程師, Sep 2018 ~ Mar 2020
+                            鴻海集團, 韌體工程師, Sep 2018 ~ Mar 2020
                             <CSSTransition
-                                classNames="socle cardTransition"
-                                in={this.state.visible}
+                                classNames="foxconn cardTransition"
+                                in={this.state.visibleWork3}
                                 timeout={300}
                                 unmountOnExit
                             >
                                 <div className="transitionPanel">
                                     Driver development
-                                    <br/>
+                                    <br />
                                     Driver unit test with Ceedling
-                                    <br/>
+                                    <br />
                                     Embedded system maintaining
                                 </div>
                             </CSSTransition>
