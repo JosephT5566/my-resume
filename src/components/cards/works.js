0f943253f1d8dbf6155d79ddb00307ea1f01ae21
Joseph Tseng <zxp930110@hotmail.com.tw>
2020-06-10 15:07:08 +0800

    Adding the content of jobs 1. Adding the content of jobs 2. Changing style

--- src/components/cards/works.js
+++ src/components/cards/works.js
@@ -27,25 +27,59 @@ class Works extends React.Component {
                         >
                             憶正科技 (MemoRight), 應用工程師, Oct 2016 ~ Mar 2017
                             <CSSTransition
-                                classNames="cardTransition"
+                                classNames="memoRight cardTransition"
                                 in={this.state.visible}
                                 timeout={300}
                                 unmountOnExit
                             >
-                                <div className="transitionPanel" />
+                                <div className="transitionPanel">
+                                    Memory controller test with Magnum system
+                                </div>
                             </CSSTransition>
                         </span>
                     </p>
                     <p>
                         <i className="circle icon"></i>
-                        <span style={{ fontSize: '22px' }}>
+                        <span
+                            onClick={this.handleClick}
+                            style={{ fontSize: '22px' }}
+                        >
                             晶兆科技 (Big-Inno), 應用工程師, May 2017 ~ Sep 2018
+                            <CSSTransition
+                                classNames="biginno cardTransition"
+                                in={this.state.visible}
+                                timeout={300}
+                                unmountOnExit
+                            >
+                                <div className="transitionPanel">
+                                    Memory controller test with Magnum system 
+                                    <br/>
+                                    MP application development in C#
+                                </div>
+                            </CSSTransition>
                         </span>
                     </p>
                     <p>
                         <i className="circle icon"></i>
-                        <span style={{ fontSize: '22px' }}>
+                        <span
+                            onClick={this.handleClick}
+                            style={{ fontSize: '22px' }}
+                        >
                             富智銓科技, 韌體工程師, Sep 2018 ~ Mar 2020
+                            <CSSTransition
+                                classNames="socle cardTransition"
+                                in={this.state.visible}
+                                timeout={300}
+                                unmountOnExit
+                            >
+                                <div className="transitionPanel">
+                                    Driver development
+                                    <br/>
+                                    Driver unit test with Ceedling
+                                    <br/>
+                                    Embedded system maintaining
+                                </div>
+                            </CSSTransition>
                         </span>
                     </p>
                 </div>
