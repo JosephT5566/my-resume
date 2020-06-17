3be45fe963aa5096acfe0885960b08330948f007
Joseph Tseng <zxp930110@hotmail.com.tw>
2020-06-17 16:14:32 +0800

    Adding `ClickAwayListener` from material ui 1. using `ClickAwayListener` element to detect if a click event happened outside of an element set props `onClickAway` to handle the event 2. auto formatting

--- src/components/cards/education.js
+++ src/components/cards/education.js
@@ -2,6 +2,7 @@ import './education.css';
 
 import React from 'react';
 import { CSSTransition } from 'react-transition-group';
+import { ClickAwayListener } from '@material-ui/core';
 
 class Education extends React.Component {
     constructor(props) {
@@ -17,64 +18,91 @@ class Education extends React.Component {
         this.setState({
             visibleEdu1: false,
             visibleEdu2: false,
-        })
-        if (isShowed) return
+        });
+        if (isShowed) return;
         this.setState((state) => {
             return {
                 [key]: !state[key],
             };
-        })
-    }
+        });
+    };
+
+    handleClickAway = (key) => () => {
+        this.setState(() => {
+            return {
+                [key]: false,
+            };
+        });
+    };
 
     render() {
         return (
             <div id="educationContainer">
                 <div className="comment">
                     <h1>學歷</h1>
-                    <hr/>
-                    <p>
-                        <i className="circle icon"></i>
-                        <span
-                            onClick={this.handleCollapse('visibleEdu1')} // or we can use arrow funcion directly
-                            style={{ fontSize: '22px' }}
-                        >
-                            國立中興大學 (NCHU), 碩士, 2014 ~ 2016
-                            <CSSTransition
-                                classNames="nchu cardTransition"
-                                in={this.state.visibleEdu1}
-                                timeout={300}
-                                unmountOnExit
+                    <hr />
+                    <ClickAwayListener
+                        onClickAway={this.handleClickAway('visibleEdu1')}
+                    >
+                        <p>
+                            <i className="circle icon"></i>
+                            <span
+                                onClick={this.handleCollapse('visibleEdu1')} // or we can use arrow funcion directly
+                                style={{ fontSize: '22px' }}
                             >
-                                <div className="transitionPanel">
-                                    <div style={ {fontWeight: 'bold'} }>電機所 · 系統與控制組 | 指導教授：許舜斌</div>
-                                    <div style={ {fontWeight: 'bold'} }>論文：光學式捲帶料件記數系統</div>
-                                    <div style={ {marginLeft: '20px'} }>在設計的測試環境上，利用清晰的捲帶盤影像，透過初步的影像處理運算，再配合設計的演算法實現元件的估算與記數。</div>
-                                    <li>孰悉影像處理演算法</li>
-                                    <li>透過 Matlab 驗證，在轉至 C++ 開發</li>
-                                    <li>透過 Qt 建立簡單使用者介面</li>
-                                </div>
-                            </CSSTransition>
-                        </span>
-                    </p>
-                    <p>
-                        <i className="circle icon"></i>
-                        <span
-                            onClick={this.handleCollapse('visibleEdu2')}
-                            style={{ fontSize: '22px' }}
-                        >
-                            國立嘉義大學 (NCYU), 學士, 2010 ~ 2014
-                            <CSSTransition
-                                classNames="ncyu cardTransition"
-                                in={this.state.visibleEdu2}
-                                timeout={300}
-                                unmountOnExit
+                                國立中興大學 (NCHU), 碩士, 2014 ~ 2016
+                                <CSSTransition
+                                    classNames="nchu cardTransition"
+                                    in={this.state.visibleEdu1}
+                                    timeout={300}
+                                    unmountOnExit
+                                >
+                                    <div className="transitionPanel">
+                                        <div style={{ fontWeight: 'bold' }}>
+                                            電機所 · 系統與控制組 |
+                                            指導教授：許舜斌
+                                        </div>
+                                        <div style={{ fontWeight: 'bold' }}>
+                                            論文：光學式捲帶料件記數系統
+                                        </div>
+                                        <div style={{ marginLeft: '20px' }}>
+                                            在設計的測試環境上，利用清晰的捲帶盤影像，透過初步的影像處理運算，再配合設計的演算法實現元件的估算與記數。
+                                        </div>
+                                        <li>孰悉影像處理演算法</li>
+                                        <li>
+                                            透過 Matlab 驗證，在轉至 C++ 開發
+                                        </li>
+                                        <li>透過 Qt 建立簡單使用者介面</li>
+                                    </div>
+                                </CSSTransition>
+                            </span>
+                        </p>
+                    </ClickAwayListener>
+                    <ClickAwayListener
+                        onClickAway={this.handleClickAway('visibleEdu2')}
+                    >
+                        <p>
+                            <i className="circle icon"></i>
+                            <span
+                                onClick={this.handleCollapse('visibleEdu2')}
+                                style={{ fontSize: '22px' }}
                             >
-                                <div className="transitionPanel">
-                                    <div style={ {fontWeight: 'bold'} }>電機工程學系</div>
-                                </div>
-                            </CSSTransition>
-                        </span>
-                    </p>
+                                國立嘉義大學 (NCYU), 學士, 2010 ~ 2014
+                                <CSSTransition
+                                    classNames="ncyu cardTransition"
+                                    in={this.state.visibleEdu2}
+                                    timeout={300}
+                                    unmountOnExit
+                                >
+                                    <div className="transitionPanel">
+                                        <div style={{ fontWeight: 'bold' }}>
+                                            電機工程學系
+                                        </div>
+                                    </div>
+                                </CSSTransition>
+                            </span>
+                        </p>
+                    </ClickAwayListener>
                 </div>
             </div>
         );
--- src/components/cards/works.js
+++ src/components/cards/works.js
@@ -2,6 +2,7 @@ import './works.css';
 
 import React from 'react';
 import { CSSTransition } from 'react-transition-group';
+import { ClickAwayListener } from '@material-ui/core';
 
 class Works extends React.Component {
     constructor(props) {
@@ -10,7 +11,7 @@ class Works extends React.Component {
             collapse: false,
             visibleWork1: false,
             visibleWork2: false,
-            visibleWork3: false
+            visibleWork3: false,
         };
     }
 
@@ -20,84 +21,125 @@ class Works extends React.Component {
             visibleWork1: false,
             visibleWork2: false,
             visibleWork3: false,
-        })
+        });
         if (isShowed) return;
         this.setState((state) => {
             return {
                 [key]: !state[key],
             };
-        })
-    }
+        });
+    };
+
+    handleClickAway = (key) => () => {
+        this.setState(() => {
+            return {
+                [key]: false,
+            };
+        });
+    };
 
     render() {
         return (
             <div id="worksContainer">
                 <div className="comment">
                     <h1>工作經歷</h1>
-                    <hr/>
-                    <p>
-                        <i className="circle icon"></i>
-                        <span
-                            onClick={this.handleCollapse('visibleWork1')} // or we can use arrow funcion directly
-                            style={{ fontSize: '22px' }}
-                        >
-                            憶正科技 (MemoRight), 應用工程師, Oct 2016 ~ Mar 2017
-                            <CSSTransition
-                                classNames="memoRight cardTransition"
-                                in={this.state.visibleWork1}
-                                timeout={300}
-                                unmountOnExit
+                    <hr />
+                    <ClickAwayListener
+                        onClickAway={this.handleClickAway('visibleWork1')}
+                    >
+                        <p>
+                            <i className="circle icon"></i>
+                            <span
+                                onClick={this.handleCollapse('visibleWork1')} // or we can use arrow funcion directly
+                                style={{ fontSize: '22px' }}
                             >
-                                <div className="transitionPanel">
-                                    <li>孰悉 eMMC Spec</li>
-                                    <li>在 MAGNUM system 上開發及維護測試記憶體控制器的testing pattern (C)</li>
-                                </div>
-                            </CSSTransition>
-                        </span>
-                    </p>
-                    <p>
-                        <i className="circle icon"></i>
-                        <span
-                            onClick={this.handleCollapse('visibleWork2')}
-                            style={{ fontSize: '22px' }}
-                        >
-                            晶兆科技 (Big-Inno), 應用工程師, May 2017 ~ Sep 2018
-                            <CSSTransition
-                                classNames="biginno cardTransition"
-                                in={this.state.visibleWork2}
-                                timeout={300}
-                                unmountOnExit
+                                憶正科技 (MemoRight), 應用工程師, Oct 2016 ~ Mar
+                                2017
+                                <CSSTransition
+                                    classNames="memoRight cardTransition"
+                                    in={this.state.visibleWork1}
+                                    timeout={300}
+                                    unmountOnExit
+                                >
+                                    <div className="transitionPanel">
+                                        <li>孰悉 eMMC Spec</li>
+                                        <li>
+                                            在 MAGNUM system
+                                            上開發及維護測試記憶體控制器的testing
+                                            pattern (C)
+                                        </li>
+                                    </div>
+                                </CSSTransition>
+                            </span>
+                        </p>
+                    </ClickAwayListener>
+                    <ClickAwayListener
+                        onClickAway={this.handleClickAway('visibleWork2')}
+                    >
+                        <p>
+                            <i className="circle icon"></i>
+                            <span
+                                onClick={this.handleCollapse('visibleWork2')}
+                                style={{ fontSize: '22px' }}
                             >
-                                <div className="transitionPanel">
-                                    <li>在 MAGNUM system 上開發及維護測試記憶體控制器的testing pattern (C)</li>
-                                    <li>協助開發 eMMC MP Tool (C#)</li>
-                                    <li>在 Linux 系統上撰寫 shell script 設計開發工具</li>
-                                </div>
-                            </CSSTransition>
-                        </span>
-                    </p>
-                    <p>
-                        <i className="circle icon"></i>
-                        <span
-                            onClick={this.handleCollapse('visibleWork3')}
-                            style={{ fontSize: '22px' }}
-                        >
-                            鴻海, 韌體工程師, Sep 2018 ~ Mar 2020
-                            <CSSTransition
-                                classNames="foxconn cardTransition"
-                                in={this.state.visibleWork3}
-                                timeout={300}
-                                unmountOnExit
+                                晶兆科技 (Big-Inno), 應用工程師, May 2017 ~ Sep
+                                2018
+                                <CSSTransition
+                                    classNames="biginno cardTransition"
+                                    in={this.state.visibleWork2}
+                                    timeout={300}
+                                    unmountOnExit
+                                >
+                                    <div className="transitionPanel">
+                                        <li>
+                                            在 MAGNUM system
+                                            上開發及維護測試記憶體控制器的testing
+                                            pattern (C)
+                                        </li>
+                                        <li>協助開發 eMMC MP Tool (C#)</li>
+                                        <li>
+                                            在 Linux 系統上撰寫 shell script
+                                            設計開發工具
+                                        </li>
+                                    </div>
+                                </CSSTransition>
+                            </span>
+                        </p>
+                    </ClickAwayListener>
+                    <ClickAwayListener
+                        onClickAway={this.handleClickAway('visibleWork3')}
+                    >
+                        <p>
+                            <i className="circle icon"></i>
+                            <span
+                                onClick={this.handleCollapse('visibleWork3')}
+                                style={{ fontSize: '22px' }}
                             >
-                                <div className="transitionPanel">
-                                    <li>用 TDD 的方式開發 Unit test 來維護 SoC driver</li>
-                                    <li>參與韌體開發流程</li>
-                                    <li>Embedded system driver 的開發與維護</li>
-                                    <li>孰悉 ARM Cortex-m4 Spec 以及用在 Embedded system 上的指令與特性</li>
-                                </div>
-                            </CSSTransition>
-                        </span>
-                    </p>
+                                鴻海, 韌體工程師, Sep 2018 ~ Mar 2020
+                                <CSSTransition
+                                    classNames="foxconn cardTransition"
+                                    in={this.state.visibleWork3}
+                                    timeout={300}
+                                    unmountOnExit
+                                >
+                                    <div className="transitionPanel">
+                                        <li>
+                                            用 TDD 的方式開發 Unit test 來維護
+                                            SoC driver
+                                        </li>
+                                        <li>參與韌體開發流程</li>
+                                        <li>
+                                            Embedded system driver 的開發與維護
+                                        </li>
+                                        <li>
+                                            孰悉 ARM Cortex-m4 Spec 以及用在
+                                            Embedded system 上的指令與特性
+                                        </li>
+                                    </div>
+                                </CSSTransition>
+                            </span>
+                        </p>
+                    </ClickAwayListener>
                 </div>
             </div>
         );
