557e69c27f9442d30249fca33a2d482fcb7a6ab8
Joseph Tseng <zxp930110@hotmail.com.tw>
2020-09-26 04:12:31 +0800

    Create Footer component 1. create Footer component and style it

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import './styles.scss';
+
+export default function Footer() {
+    return (
+        <footer id="footer" class="wrapper style1-alt">
+            <div class="inner">
+                <ul class="menu">
+                    <li>&copy; Untitled. All rights reserved.</li>
+                    <li>
+                        Design: <a href="http://html5up.net">HTML5 UP</a>
+                    </li>
+                </ul>
+            </div>
+        </footer>
+    );
+}
