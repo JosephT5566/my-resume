7a2757ba37b98ebbde765d5113662e66d3ef47d7
Joseph Tseng <zxp930110@hotmail.com.tw>
2020-11-07 03:20:26 +0800

    Feat: Change Accordion usage 1. use Accordion component from material-ui

--- src/components/InfoCards/Educations/index.jsx
+++ src/components/InfoCards/Educations/index.jsx
@@ -1,73 +1,68 @@
 import React, { useState } from 'react';
-import { CSSTransition } from 'react-transition-group';
 import { ClickAwayListener } from '@material-ui/core';
+import Accordion from '@material-ui/core/Accordion';
+import AccordionDetails from '@material-ui/core/AccordionDetails';
+import AccordionSummary from '@material-ui/core/AccordionSummary';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+
 import './education.scss';
 
 export default function Education() {
-    const [visible, setVisible] = useState({ visibleEdu1: false, visibleEdu2: false });
+    const [expanded, setExpanded] = useState('');
 
-    const handleCollapse = (key) => {
-        let isShowed = visible[key];
-        setVisible({
-            visibleEdu1: false,
-            visibleEdu2: false,
+    const handleClickAwayPanel1 = () => {
+        setExpanded((prev) => {
+            return prev === 'panel1' ? false : prev;
         });
-        if (isShowed) return;
-        setVisible((prev) => ({ ...prev, [key]: !prev[key] }));
     };
 
-    const handleClickAway = (key) => {
-        setVisible((prev) => ({ ...prev, [key]: false }));
+    const handleClickAwayPanel2 = () => {
+        setExpanded((prev) => {
+            return prev === 'panel2' ? false : prev;
+        });
+    };
+
+    const handleChange = (panel) => (event, isExpanded) => {
+        setExpanded(isExpanded ? panel : false);
     };
 
     return (
         <div id="educationContainer">
             <h1>學歷</h1>
-            <ClickAwayListener onClickAway={() => handleClickAway('visibleEdu1')}>
-                <div className="list">
-                    <i className="circle icon"></i>
-                    <span
-                        onClick={() => handleCollapse('visibleEdu1')} // or we can use arrow funcion directly
-                        style={{ fontSize: '22px' }}
-                    >
-                        國立中興大學 (NCHU), 碩士, 2014 ~ 2016
-                        <CSSTransition
-                            classNames="nchu cardTransition"
-                            in={visible.visibleEdu1}
-                            timeout={300}
-                            unmountOnExit
-                        >
-                            <div className="transitionPanel">
-                                <div style={{ fontWeight: 'bold' }}>電機所 · 系統與控制組 | 指導教授：許舜斌</div>
-                                <div style={{ fontWeight: 'bold' }}>論文：光學式捲帶料件記數系統</div>
-                                <div style={{ marginLeft: '20px' }}>
-                                    在設計的測試環境上，利用清晰的捲帶盤影像，透過初步的影像處理運算，再配合設計的演算法實現元件的估算與記數。
-                                </div>
-                                <li>孰悉影像處理演算法</li>
-                                <li>透過 Matlab 驗證，在轉至 C++ 開發</li>
-                                <li>透過 Qt 建立簡單使用者介面</li>
+            <ClickAwayListener onClickAway={handleClickAwayPanel1}>
+                <Accordion expanded={expanded === 'panel1'} onChange={handleChange('panel1')}>
+                    <AccordionSummary expandIcon={<ExpandMoreIcon fontSize="large"/>}>
+                        <div className="accordionTitle">
+                            國立中興大學 (NCHU), 碩士, 2014 ~ 2016
+                        </div>
+                    </AccordionSummary>
+                    <AccordionDetails>
+                        <div className="transitionPanel">
+                            <div style={{ fontWeight: 'bold' }}>電機所 · 系統與控制組 | 指導教授：許舜斌</div>
+                            <div style={{ fontWeight: 'bold' }}>論文：光學式捲帶料件記數系統</div>
+                            <div style={{ marginLeft: '20px' }}>
+                                在設計的測試環境上，利用清晰的捲帶盤影像，透過初步的影像處理運算，再配合設計的演算法實現元件的估算與記數。
                             </div>
-                        </CSSTransition>
-                    </span>
-                </div>
+                            <li>孰悉影像處理演算法</li>
+                            <li>透過 Matlab 驗證，在轉至 C++ 開發</li>
+                            <li>透過 Qt 建立簡單使用者介面</li>
+                        </div>
+                    </AccordionDetails>
+                </Accordion>
             </ClickAwayListener>
-            <ClickAwayListener onClickAway={() => handleClickAway('visibleEdu2')}>
-                <div className="list">
-                    <i className="circle icon"></i>
-                    <span onClick={() => handleCollapse('visibleEdu2')} style={{ fontSize: '22px' }}>
-                        國立嘉義大學 (NCYU), 學士, 2010 ~ 2014
-                        <CSSTransition
-                            classNames="ncyu cardTransition"
-                            in={visible.visibleEdu2}
-                            timeout={300}
-                            unmountOnExit
-                        >
-                            <div className="transitionPanel">
-                                <div style={{ fontWeight: 'bold' }}>電機工程學系</div>
-                            </div>
-                        </CSSTransition>
-                    </span>
-                </div>
+            <ClickAwayListener onClickAway={handleClickAwayPanel2}>
+                <Accordion expanded={expanded === 'panel2'} onChange={handleChange('panel2')}>
+                    <AccordionSummary expandIcon={<ExpandMoreIcon fontSize="large"/>}>
+                        <div className="accordionTitle">
+                            國立嘉義大學 (NCYU), 學士, 2010 ~ 2014
+                        </div>
+                    </AccordionSummary>
+                    <AccordionDetails>
+                        <div className="transitionPanel">
+                            <div style={{ fontWeight: 'bold' }}>電機工程學系</div>
+                        </div>
+                    </AccordionDetails>
+                </Accordion>
             </ClickAwayListener>
         </div>
     );
--- src/components/InfoCards/Works/index.jsx
+++ src/components/InfoCards/Works/index.jsx
@@ -1,93 +1,83 @@
 import React, { useState } from 'react';
 import { CSSTransition } from 'react-transition-group';
 import { ClickAwayListener } from '@material-ui/core';
+import Accordion from '@material-ui/core/Accordion';
+import AccordionDetails from '@material-ui/core/AccordionDetails';
+import AccordionSummary from '@material-ui/core/AccordionSummary';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
 import './works.scss';
 
 export default function Works() {
-    const [visible, setVisible] = useState({ visibleWork1: false, visibleWork2: false, visibleWork3: false });
+    const [expanded, setExpanded] = useState('');
 
-    const handleCollapse = (key) => {
-        let isShowed = visible[key];
-        setVisible({
-            visibleWork1: false,
-            visibleWork2: false,
-            visibleWork3: false,
+    const handleClickAwayPanel1 = () => {
+        setExpanded((prev) => {
+            return prev === 'panel1' ? false : prev;
         });
-        if (isShowed) return;
-        setVisible((prev) => ({ ...prev, [key]: !prev[key] }));
     };
 
-    const handleClickAway = (key) => {
-        setVisible((prev) => ({ ...prev, [key]: false }));
+    const handleClickAwayPanel2 = () => {
+        setExpanded((prev) => {
+            return prev === 'panel2' ? false : prev;
+        });
+    };
+
+    const handleClickAwayPanel3 = () => {
+        setExpanded((prev) => {
+            return prev === 'panel3' ? false : prev;
+        });
+    };
+
+    const handleChange = (panel) => (event, isExpanded) => {
+        setExpanded(isExpanded ? panel : false);
     };
 
     return (
         <div id="worksContainer">
             <h1>工作經歷</h1>
 
-            <ClickAwayListener onClickAway={() => handleClickAway('visibleWork1')}>
-                <div className="list">
-                    <i className="circle icon"></i>
-                    <span
-                        onClick={() => handleCollapse('visibleWork1')} // or we can use arrow funcion directly
-                        style={{ fontSize: '22px' }}
-                    >
-                        憶正科技 (MemoRight), 應用工程師, Oct 2016 ~ Mar 2017
-                        <CSSTransition
-                            classNames="memoRight cardTransition"
-                            in={visible.visibleWork1}
-                            timeout={300}
-                            unmountOnExit
-                        >
-                            <div className="transitionPanel">
-                                <li>孰悉 eMMC Spec</li>
-                                <li>在 MAGNUM system 上開發及維護測試記憶體控制器的testing pattern (C)</li>
-                            </div>
-                        </CSSTransition>
-                    </span>
-                </div>
+            <ClickAwayListener onClickAway={handleClickAwayPanel1}>
+                <Accordion expanded={expanded === 'panel1'} onChange={handleChange('panel1')}>
+                    <AccordionSummary expandIcon={<ExpandMoreIcon fontSize="large" />}>
+                        <div className="accordionTitle">憶正科技 (MemoRight), 應用工程師, Oct 2016 ~ Mar 2017</div>
+                    </AccordionSummary>
+                    <AccordionDetails>
+                        <div className="transitionPanel">
+                            <li>孰悉 eMMC Spec</li>
+                            <li>在 MAGNUM system 上開發及維護測試記憶體控制器的testing pattern (C)</li>
+                        </div>
+                    </AccordionDetails>
+                </Accordion>
             </ClickAwayListener>
-            <ClickAwayListener onClickAway={() => handleClickAway('visibleWork2')}>
-                <div className="list">
-                    <i className="circle icon"></i>
-                    <span onClick={() => handleCollapse('visibleWork2')} style={{ fontSize: '22px' }}>
-                        晶兆科技 (Big-Inno), 應用工程師, May 2017 ~ Sep 2018
-                        <CSSTransition
-                            classNames="biginno cardTransition"
-                            in={visible.visibleWork2}
-                            timeout={300}
-                            unmountOnExit
-                        >
-                            <div className="transitionPanel">
-                                <li>在 MAGNUM system 上開發及維護測試記憶體控制器的testing pattern (C)</li>
-                                <li>協助開發 eMMC MP Tool (C#)</li>
-                                <li>在 Linux 系統上撰寫 shell script 設計開發工具</li>
-                            </div>
-                        </CSSTransition>
-                    </span>
-                </div>
+            <ClickAwayListener onClickAway={handleClickAwayPanel2}>
+                <Accordion expanded={expanded === 'panel2'} onChange={handleChange('panel2')}>
+                    <AccordionSummary expandIcon={<ExpandMoreIcon fontSize="large" />}>
+                        <div className="accordionTitle">晶兆科技 (Big-Inno), 應用工程師, May 2017 ~ Sep 2018</div>
+                    </AccordionSummary>
+                    <AccordionDetails>
+                        <div className="transitionPanel">
+                            <li>在 MAGNUM system 上開發及維護測試記憶體控制器的testing pattern (C)</li>
+                            <li>協助開發 eMMC MP Tool (C#)</li>
+                            <li>在 Linux 系統上撰寫 shell script 設計開發工具</li>
+                        </div>
+                    </AccordionDetails>
+                </Accordion>
             </ClickAwayListener>
-            <ClickAwayListener onClickAway={() => handleClickAway('visibleWork3')}>
-                <div className="list">
-                    <i className="circle icon"></i>
-                    <span onClick={() => handleCollapse('visibleWork3')} style={{ fontSize: '22px' }}>
-                        鴻海, 韌體工程師, Sep 2018 ~ Mar 2020
-                        <CSSTransition
-                            classNames="foxconn cardTransition"
-                            in={visible.visibleWork3}
-                            timeout={300}
-                            unmountOnExit
-                        >
-                            <div className="transitionPanel">
-                                <li>用 TDD 的方式開發 Unit test 來維護 SoC driver</li>
-                                <li>參與韌體開發流程</li>
-                                <li>Embedded system driver 的開發與維護</li>
-                                <li>孰悉 ARM Cortex-m4 Spec 以及用在 Embedded system 上的指令與特性</li>
-                            </div>
-                        </CSSTransition>
-                    </span>
-                </div>
+            <ClickAwayListener onClickAway={handleClickAwayPanel3}>
+                <Accordion expanded={expanded === 'panel3'} onChange={handleChange('panel3')}>
+                    <AccordionSummary expandIcon={<ExpandMoreIcon fontSize="large" />}>
+                        <div className="accordionTitle">鴻海, 韌體工程師, Sep 2018 ~ Mar 2020</div>
+                    </AccordionSummary>
+                    <AccordionDetails>
+                        <div className="transitionPanel">
+                            <li>用 TDD 的方式開發 Unit test 來維護 SoC driver</li>
+                            <li>參與韌體開發流程</li>
+                            <li>Embedded system driver 的開發與維護</li>
+                            <li>孰悉 ARM Cortex-m4 Spec 以及用在 Embedded system 上的指令與特性</li>
+                        </div>
+                    </AccordionDetails>
+                </Accordion>
             </ClickAwayListener>
         </div>
     );
