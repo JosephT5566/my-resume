4b2cf4753cd4d776bb9168c6fd95e48ec19ec478
Your JosephTseng <zxp930110@hotmail.com.tw>
2021-03-23 12:10:51 +0800

    add ClickAwayListener, set navbar to right side

--- src/components/Navigation/index.jsx
+++ src/components/Navigation/index.jsx
@@ -5,6 +5,7 @@ import PositionContext from '../../contexts/PositionContext';
 import IndexContext from '../../contexts/IndexContext';
 import ScrollyButton from './ScrollyButton';
 import MenuIcon from '@material-ui/icons/Menu';
+import ClickAwayListener from '@material-ui/core/ClickAwayListener'
 
 import './styles.scss';
 
@@ -49,99 +50,119 @@ export default function Navigation() {
         false
     );
 
+    const handleClickAway = () => {
+        if (active) {
+            setActive(false);
+        }
+    }
+
+	const handleButtonClicked = () => {
+		setActive(false);
+	};
+
     return (
-        <section id="sidebar" className={`${activeClass}`}>
-            <button
-                id="menuButton"
-                onClick={() => {
-                    setActive((prev) => !prev);
-                }}
-            >
-                <MenuIcon className="icon" />
-            </button>
-            <div className="inner">
-                <nav>
-                    <ul>
-                        <li>
-                            <ScrollyButton
-                                label="Info"
-                                index={0}
-                                isClicked={isClicked}
-                                onClick={() => {
-                                    setIsClicked(true);
-                                    scroll.scrollTo(positions[0] + basePoint - 1);
-                                }}
-                            />
-                        </li>
-                        <li>
-                            <ScrollyButton
-                                label="Educations"
-                                index={1}
-                                isClicked={isClicked}
-                                onClick={() => {
-                                    setIsClicked(true);
-                                    scroll.scrollTo(positions[1] + basePoint - 1);
-                                }}
-                            />
-                        </li>
-                        <li>
-                            <ScrollyButton
-                                label="Works"
-                                index={2}
-                                isClicked={isClicked}
-                                onClick={() => {
-                                    setIsClicked(true);
-                                    scroll.scrollTo(positions[2] + basePoint - 1);
-                                }}
-                            />
-                        </li>
-                        <li>
-                            <ScrollyButton
-                                label="Expertises"
-                                index={3}
-                                isClicked={isClicked}
-                                onClick={() => {
-                                    setIsClicked(true);
-                                    scroll.scrollTo(positions[3] + basePoint - 1);
-                                }}
-                            />
-                        </li>
-                        <li>
-                            <ScrollyButton
-                                label="Skills"
-                                index={4}
-                                isClicked={isClicked}
-                                onClick={() => {
-                                    setIsClicked(true);
-                                    scroll.scrollTo(positions[4] + basePoint - 1);
-                                }}
-                            />
-                        </li>
-                        <li>
-                            <ScrollyButton
-                                label="Introduction"
-                                index={5}
-                                isClicked={isClicked}
-                                onClick={() => {
-                                    setIsClicked(true);
-                                    scroll.scrollTo(positions[5] + basePoint - 1);
-                                }}
-                            />
-                        </li>
-                        <li>
-                            <ScrollyButton
-                                label="Collections"
-                                index={6}
-                                isClicked={isClicked}
-                                onClick={() => {
-                                    setIsClicked(true);
-                                    scroll.scrollTo(positions[6] + basePoint - 1);
-                                }}
-                            />
-                        </li>
-                    </ul>
-                </nav>
-            </div>
-        </section>
+        <ClickAwayListener onClickAway={handleClickAway}>
+            <section id="sidebar" className={`${activeClass}`}>
+                <button
+                    id="menuButton"
+                    onClick={() => {
+                        setActive((prev) => !prev);
+                    }}
+                >
+                    <MenuIcon className="icon" />
+                </button>
+                <div className="inner">
+                    <nav>
+                        <ul>
+                            <li>
+                                <ScrollyButton
+                                    label="Info"
+                                    index={0}
+                                    isClicked={isClicked}
+                                    onClick={() => {
+                                        setIsClicked(true);
+                                        handleButtonClicked()
+                                        scroll.scrollTo(positions[0] + basePoint - 1);
+                                    }}
+                                />
+                            </li>
+                            <li>
+                                <ScrollyButton
+                                    label="Educations"
+                                    index={1}
+                                    isClicked={isClicked}
+                                    onClick={() => {
+                                        setIsClicked(true);
+                                        handleButtonClicked()
+                                        scroll.scrollTo(positions[1] + basePoint - 1);
+                                    }}
+                                />
+                            </li>
+                            <li>
+                                <ScrollyButton
+                                    label="Works"
+                                    index={2}
+                                    isClicked={isClicked}
+                                    onClick={() => {
+                                        setIsClicked(true);
+                                        handleButtonClicked()
+                                        scroll.scrollTo(positions[2] + basePoint - 1);
+                                    }}
+                                />
+                            </li>
+                            <li>
+                                <ScrollyButton
+                                    label="Expertises"
+                                    index={3}
+                                    isClicked={isClicked}
+                                    onClick={() => {
+                                        setIsClicked(true);
+                                        handleButtonClicked()
+                                        scroll.scrollTo(positions[3] + basePoint - 1);
+                                    }}
+                                />
+                            </li>
+                            <li>
+                                <ScrollyButton
+                                    label="Skills"
+                                    index={4}
+                                    isClicked={isClicked}
+                                    onClick={() => {
+                                        setIsClicked(true);
+                                        handleButtonClicked()
+                                        scroll.scrollTo(positions[4] + basePoint - 1);
+                                    }}
+                                />
+                            </li>
+                            <li>
+                                <ScrollyButton
+                                    label="Introduction"
+                                    index={5}
+                                    isClicked={isClicked}
+                                    onClick={() => {
+                                        setIsClicked(true);
+                                        handleButtonClicked()
+                                        scroll.scrollTo(positions[5] + basePoint - 1);
+                                    }}
+                                />
+                            </li>
+                            <li>
+                                <ScrollyButton
+                                    label="Collections"
+                                    index={6}
+                                    isClicked={isClicked}
+                                    onClick={() => {
+                                        setIsClicked(true);
+                                        handleButtonClicked()
+                                        scroll.scrollTo(positions[6] + basePoint - 1);
+                                    }}
+                                />
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+            </section>
+        </ClickAwayListener>
+        
     );
 }
