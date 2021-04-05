bd242386c97e24d3b0c382e50bd2e0b7c70d5d3e
JosephTseng <zxp930110@hotmail.com.tw>
2021-04-05 23:22:57 +0800

    add new collection

--- src/components/InfoCards/Collections/index.jsx
+++ src/components/InfoCards/Collections/index.jsx
@@ -4,30 +4,45 @@ import collectionImage1 from 'assets/images/collection1.jpg';
 
 import './collection.scss';
 
+const Preview = ({ title, description, image, url, ...props }) => (
+    <a className="section" href={url}>
+        <div className="image">
+            <img src={image} alt="collectionImg" />
+        </div>
+        <div className="description">
+            <h2>{title}</h2>
+            <h3>{description}</h3>
+            {props.children}
+        </div>
+    </a>
+);
+
 const Collection = () => {
     return (
         <div id="collectionContainer">
             <h1>作品</h1>
             <div className="content">
-                <div className="section">
-                    <div className="image">
-                        <img
-                            src={collectionImage1}
-                            alt="collectionImage1"
-                            onClick={() => {
-                                window.open('https://josify.herokuapp.com/register.php');
-                            }}
-                        />
-                    </div>
-                    <div className="description">
-                        <h2>Josify</h2>
-                        <h3>參照Spotify介面規劃的音樂App</h3>
-                        <li>PHP</li>
-                        <li>JavaScript / JQuery / Ajax</li>
-                        <li>HTML5</li>
-                        <li>MySQL</li>
-                    </div>
-                </div>
+                <Preview
+                    title={'Josify'}
+                    description={'參照Spotify介面規劃的音樂App'}
+                    image={collectionImage1}
+                    url={'https://josify.herokuapp.com/register.php'}
+                >
+                    <li>PHP</li>
+                    <li>JavaScript / JQuery / Ajax</li>
+                    <li>HTML5</li>
+                    <li>MySQL</li>
+                </Preview>
+                <Preview
+                    title={'Megaport Festival Selector'}
+                    description={'2021大港開唱，看團選擇器'}
+                    image={'https://i.imgur.com/OgYq1S8.png'}
+                    url={'https://josepht5566.github.io/musicFest'}
+                >
+                    <li>React</li>
+                    <li>Material ui styles</li>
+                    <li>gh pages</li>
+                </Preview>
             </div>
         </div>
     );
